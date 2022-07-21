fe98fb3a945c602d4a94852cf4363708013f8f6c
Setas <setas@test.com>
2022-07-21 19:13:02 +0200

    remove console log

--- vue_wavesurfer_player/src/components/Wavesurfer.vue
+++ vue_wavesurfer_player/src/components/Wavesurfer.vue
@@ -2,63 +2,62 @@
 import { defineComponent } from "vue";
 import WaveSurfer from "wavesurfer.js";
 import { WavesurferOptions } from "./WavesurferOptions";
+import PauseButtonIcon from "../../Icons/PauseButtonIcon.vue";
+import PlayButtonIcon from "../../Icons/PlayButtonIcon.vue";
 
 export default defineComponent({
-  props: {
-    src: String,
-    options: Object as () => WavesurferOptions,
-    durationValue: String,
-    effectName: String,
-    srcBase64: String,
-  },
-  data() {
-    return {
-      playing: false,
-    };
-  },
-  mounted() {
-    this.wavesurfer = WaveSurfer.create(this.options);
-    if (this.src === undefined) {
-      this.wavesurfer.loadBlob(this.toBlob(this.srcBase64));
-    } else {
-      this.wavesurfer.load(this.src);
-    }
-    this.wavesurfer.on("pause", () => {
-      this.playing = false;
-    });
-    this.wavesurfer.on("finish", () => {
-      this.playing = true;
-    });
-  },
-  methods: {
-    play() {
-      this.wavesurfer.play();
-      this.playing = true;
+    props: {
+        src: String,
+        options: Object as () => WavesurferOptions,
+        durationValue: String,
+        effectName: String,
+        srcBase64: String,
     },
-    stop() {
-      this.wavesurfer.pause();
+    data() {
+        return {
+            playing: false,
+        };
     },
-    getId(option: WavesurferOptions) {
-      return option.container.replace("#", "");
+    mounted() {
+        this.wavesurfer = WaveSurfer.create(this.options);
+        if (this.src === undefined) {
+            this.wavesurfer.loadBlob(this.toBlob(this.srcBase64));
+        }
+        else {
+            this.wavesurfer.load(this.src);
+        }
+        this.wavesurfer.on("pause", () => {
+            this.playing = false;
+        });
+        this.wavesurfer.on("finish", () => {
+            this.playing = true;
+        });
     },
-    toBlob(b64: string): Blob {
-      const byteCharacters = atob(b64);
-      const byteNumbers = new Array(byteCharacters.length);
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-      }
-      const byteArray = new Uint8Array(byteNumbers);
-      return new Blob([byteArray], { type: "audio/wav" });
+    methods: {
+        play() {
+            this.wavesurfer.play();
+            this.playing = true;
+        },
+        stop() {
+            this.wavesurfer.pause();
+        },
+        getId(option: WavesurferOptions) {
+            return option.container.replace("#", "");
+        },
+        toBlob(b64: string): Blob {
+            const byteCharacters = atob(b64);
+            const byteNumbers = new Array(byteCharacters.length);
+            for (let i = 0; i < byteCharacters.length; i++) {
+                byteNumbers[i] = byteCharacters.charCodeAt(i);
+            }
+            const byteArray = new Uint8Array(byteNumbers);
+            return new Blob([byteArray], { type: "audio/wav" });
+        },
     },
-  },
-  created() {
-    console.log(this.options);
-  },
+    components: { PauseButtonIcon, PlayButtonIcon }
 });
 </script>
 <style>
-@import url("https://css.gg/play-button-o.css");
-@import url("https://css.gg/play-pause-o.css");
 .wave-surfer-player-container {
   width: 100%;
   display: flex;
@@ -93,10 +92,10 @@ export default defineComponent({
 <template>
   <div class="wave-surfer-player-container">
     <div class="wave-surfer-play-btn" @click="play" v-show="!playing">
-      <i class="wave-surfer-icon gg-play-button-o"></i>
+      <PlayButtonIcon class="wave-surfer-icon gg-play-button-o"/>      
     </div>
     <div class="wave-surfer-play-btn" @click="stop" v-show="playing">
-      <i class="wave-surfer-icon gg-play-pause-o"></i>
+      <PauseButtonIcon class="wave-surfer-icon gg-play-pause-o"/>     
     </div>
     <div class="wave-surfer-container">
       <div class="wave-surfer-name-container">{{ effectName }}</div>
